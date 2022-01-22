0f37eded6a962479951d231a18195aaa1423249c
Joanna May <joannamadisonmay@gmail.com>
2022-01-22 13:39:00 -0600

    Add pattern for extending markdown docs

--- lib/src/recipe_engine.dart
+++ lib/src/recipe_engine.dart
@@ -1,5 +1,36 @@
 import 'package:markdown/markdown.dart';
 
+/// {@template recipe_node_visitor}
+/// A [NodeVisitor] used to visit nodes created by parsing
+/// a markdown file with the recipe engine extensions.
+abstract class RecipeNodeVisitor implements NodeVisitor {
+  /// Called when a reference node has been reached.
+  void visitReference(Reference reference);
+}
+
+/// {@template reference}
+/// Reference to another recipe, ingredient, or supply.
+/// {@endtemplate}
+class Reference implements Node {
+  /// {@macro reference}
+  const Reference(this.textContent);
+
+  @override
+  final String textContent;
+
+  @override
+  void accept(covariant RecipeNodeVisitor visitor) =>
+      visitor.visitReference(this);
+}
+
+class RecipeHtmlRender extends HtmlRenderer implements RecipeNodeVisitor {
+  /// Visit a reference node.
+  @override
+  void visitReference(Reference reference) {
+    buffer.write(reference.textContent);
+  }
+}
+
 /// {@template recipe_engine}
 /// Examine Markdown recipe files.
 /// {@endtemplate}
@@ -13,8 +44,9 @@ class RecipeEngine {
   /// Parse the recipe text.
   List<Node> parse() {
     final document = Document(
-      // Unresolved links are likely references to ingredients or cooking supplies.
-      linkResolver: (String name, [String? title]) => UnparsedContent(name),
+      // Unresolved links are most likely references to ingredients,
+      // recipes, or cooking supplies.
+      linkResolver: (String name, [String? title]) => Reference(name),
     );
     // Replace windows line endings with unix line endings, and split.
     final lines = [...markdown.replaceAll('\r\n', '\n').split('\n'), '\n'];
