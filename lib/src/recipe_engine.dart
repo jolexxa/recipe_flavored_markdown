14afdee653bde47ce7b25c6dab94f96b46b375df
Joanna May <joannamadisonmay@gmail.com>
2022-01-22 14:57:12 -0600

    Add basic quantity and reference detection

--- lib/src/recipe_engine.dart
+++ lib/src/recipe_engine.dart
@@ -6,6 +6,9 @@ import 'package:markdown/markdown.dart';
 abstract class RecipeNodeVisitor implements NodeVisitor {
   /// Called when a reference node has been reached.
   void visitReference(Reference reference);
+
+  /// Called when a quantity node has been reached.
+  void visitQuantity(Quantity quantity);
 }
 
 /// {@template reference}
@@ -23,6 +26,22 @@ class Reference implements Node {
       visitor.visitReference(this);
 }
 
+/// {@template quantity}
+/// Represents a cooking quantity.
+/// {@endtemplate}
+class Quantity implements Node {
+  /// {@macro quantity}
+
+  const Quantity(this.textContent);
+
+  @override
+  final String textContent;
+
+  @override
+  void accept(covariant RecipeNodeVisitor visitor) =>
+      visitor.visitQuantity(this);
+}
+
 /// {@template recipe_html_renderer}
 /// An HTML renderer that can render recipe-flavored markdown.
 /// {@endtemplate}
@@ -34,6 +53,33 @@ class RecipeHtmlRenderer extends HtmlRenderer implements RecipeNodeVisitor {
   @override
   void visitReference(Reference reference) {
     buffer.write(reference.textContent);
+  }
+
+  @override
+  void visitQuantity(Quantity quantity) {
+    buffer.write(quantity.textContent);
+  }
+}
+
+/// {@template quantity_syntax}
+/// Represents a cooking quantity.
+/// {@endtemplate}
+class QuantitySyntax extends InlineSyntax {
+  /// {@macro quantity_syntax}
+  QuantitySyntax() : super(_pattern);
+
+  static const String _quantifier =
+      r'(?:\d+(?:\.\d+)?)(?:\s*\/\s*(?:\d+(?:\.\d+)?))?';
+  static const String _units = '(?:'
+      'tsp(?:s?)|tbsp(?:s?)|oz|cup(?:s?)|pint(?:s?)|quart(?:s?)|'
+      'gal(?:s?)|gallon(?:s?)|lb(?:s?)|kg|g|ml|l'
+      ')';
+  static const String _pattern = '$_quantifier\\s+$_units';
+
+  @override
+  bool onMatch(InlineParser parser, Match match) {
+    parser.addNode(Quantity(match.group(0)!));
+    return true;
   }
 }
 
@@ -53,6 +99,9 @@ class RecipeEngine {
       // Unresolved links are most likely references to ingredients,
       // recipes, or cooking supplies.
       linkResolver: (String name, [String? title]) => Reference(name),
+      inlineSyntaxes: [
+        QuantitySyntax(),
+      ],
     );
     // Replace windows line endings with unix line endings, and split.
     final lines = [...markdown.replaceAll('\r\n', '\n').split('\n'), '\n'];
